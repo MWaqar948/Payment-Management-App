239d6f633eb1fec9192a34fbc34728577e5f8029
M.Waqar <waqaruos92@gmail.com>
2023-11-29 21:21:06 +0500

    database schema designed

--- database/migrations/2023_11_28_172940_create_roles_table.php
+++ database/migrations/2023_11_28_172940_create_roles_table.php
@@ -11,16 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            // $table->decimal('total_amount', 10, 2);
-            // $table->decimal('doctor_fee', 10, 2);
-            $table->unsignedBigInteger('role_id');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -30,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
---
+++ database/migrations/2023_11_28_173033_create_appointments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('booking_time');
+            $table->decimal('doctor_fee_amount', 10, 2);
+            $table->unsignedBigInteger('client_id');
+            $table->unsignedBigInteger('doctor_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
